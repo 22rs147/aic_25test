b980a5b1b644205b5bc0245a09c5cae3b9c232c3
22rs147 <145908063+22rs147@users.noreply.github.com>
2025-07-16 14:09:14 +0900

    会員情報保存機能の実装。
    会員情報編集から保存機能が使えなかったのを使えるようにした。

--- src/controllers/Member.php
+++ src/controllers/Member.php
@@ -64,4 +64,45 @@ class Member extends Controller
 
         $this->view->render('mbr_input.php', $data);
     }
+
+    // 会員情報保存
+    public function saveAction()
+    {
+        // 1. 権限チェック
+        $security = new Security();
+        $security->require('login'); // ログインは必須
+
+        // POSTデータからIDを取得。なければ一覧へリダイレクト
+        if (!isset($_POST['id']) || empty($_POST['id'])) {
+            $this->view->redirect('index.php?to=mbr&do=list');
+            return;
+        }
+        $mbr_id = (int)$_POST['id'];
+
+        // 管理者でなければ、自分の情報しか保存できない
+        $user = new User();
+        if (!$user->isAdmin()) {
+            $security->require('owner', $mbr_id);
+        }
+
+        // 2. 保存するデータをPOSTから取得 (mbr_input.phpのフォーム項目に対応)
+        $data = [
+            'id'        => $mbr_id,
+            'ja_name'   => $_POST['ja_name']   ?? null,
+            'ja_yomi'   => $_POST['ja_yomi']   ?? null,
+            'en_name'   => $_POST['en_name']   ?? null,
+            'en_yomi'   => $_POST['en_yomi']   ?? null,
+            'email'     => $_POST['email']     ?? null,
+            'tel_no'    => $_POST['tel_no']    ?? null,
+            'sex'       => $_POST['sex']       ?? null,
+            'dept_name' => $_POST['dept_name'] ?? null,
+            'dept_code' => $_POST['dept_code'] ?? null,
+        ];
+
+        // 3. モデルのwriteメソッドでデータを保存
+        $this->model->write($data);
+
+        // 4. 保存後、詳細ページにリダイレクト
+        $this->view->redirect('index.php?to=mbr&do=detail&id=' . $mbr_id);
+    }
 }
